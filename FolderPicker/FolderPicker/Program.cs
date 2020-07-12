244ab5714c45babb8f7975b98d649734ffa254f0
ison <hidekiisobe9@gmail.com>
2020-07-12 16:03:37 +0900

    リリース

--- FolderPicker/FolderPicker/Program.cs
+++ FolderPicker/FolderPicker/Program.cs
@@ -16,35 +16,66 @@ namespace FolderPicker
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            bool? isSelected = null;
 
-            var args = Environment.GetCommandLineArgs();
-
-            string title = args[1].Trim('"');
-            string InitialDir = args[2].Trim('"');
-            string outpuFile = args[3].Trim('"');
-
-            if (string.IsNullOrEmpty(title))
+            try
             {
-                title = "フォルダ選択";
-            }
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
 
-            var dialog = new CommonOpenFileDialog(title);
-            dialog.IsFolderPicker = true; // フォルダ選択
-            dialog.InitialDirectory = InitialDir;
+                var args = Environment.GetCommandLineArgs();
 
-            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
-            {
-                var selectedDirPath = dialog.FileName;
+                string title = args[1].Trim('"');
+                string InitialDir = args[2].Trim('"');
+                string outpuFile = args[3].Trim('"');
 
-                if (!Directory.Exists(Path.GetDirectoryName(outpuFile)))
+                if (string.IsNullOrEmpty(title))
                 {
-                    Directory.CreateDirectory(Path.GetDirectoryName(outpuFile));
+                    title = "フォルダ選択";
                 }
 
-                File.WriteAllText(outpuFile, selectedDirPath);
+                var dialog = new CommonOpenFileDialog(title);
+                dialog.IsFolderPicker = true; // フォルダ選択
+                dialog.InitialDirectory = InitialDir;
+
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    var selectedDirPath = dialog.FileName;
+
+                    if (!Directory.Exists(Path.GetDirectoryName(outpuFile)))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(outpuFile));
+                    }
+
+                    File.WriteAllText(outpuFile, selectedDirPath);
+
+                    isSelected = true;
+                } else
+                {
+                    isSelected = false;
+                }
             }
+            catch (Exception)
+            {
+                isSelected = null;
+            }
+
+            if (isSelected == null)
+            {
+                // 予期せぬエラー
+                Environment.Exit(1);
+            }
+            else if (isSelected.Value)
+            {
+                // 正常（選択）
+                Environment.Exit(0);
+            }
+            else
+            {
+                // 正常（キャンセル）
+                Environment.Exit(-1);
+            }
+
         }
     }
 }
